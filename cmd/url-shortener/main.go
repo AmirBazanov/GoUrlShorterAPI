5d02f6a3c0e0105ec56187745c5c08fd45cb711d
AmirBazanov <flash5651311@gmail.com>
2024-06-10 12:49:11 +0300

    Added sqlite DB

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"ShorterAPI/internal/config"
+	"ShorterAPI/internal/lib/logger/sl"
+	"ShorterAPI/internal/storage/sqlite"
 	"log/slog"
 	"os"
 )
@@ -17,6 +19,13 @@ func main() {
 	log := setupLogger(cfg.Env)
 	log.Info("starting server")
 	log.Debug("debug logging enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("error opening storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
